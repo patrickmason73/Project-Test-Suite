6be9f58de122a69ef986cefc5c002fd41c2f143b
Patrick Mason <patrickmason73@gmail.com>
2024-03-19 14:31:40 -0500

    added header.js, app.js usercontext added

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,11 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import Test from "./Test";
 import { UserContext } from "../contexts/UserContext";
 import { Routes, Route, useNavigate } from 'react-router-dom'
 
 function App() {
+
+  const {currentUser, setCurrentUser} = useContext(UserContext)
 
   return (
     <div>
