05d3207b79a335e4854ab3f9307f6b8eca7068fd
Patrick Mason <patrickmason73@gmail.com>
2024-03-21 15:11:27 -0500

    index.js adjusted to createRoot for React 18

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
 import {BrowserRouter as Router} from 'react-router-dom';
 import { UserProvider } from './contexts/UserContext';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <Router>
     <UserProvider>
-      <App />
+    <App />
     </UserProvider>
-  </Router>,
-  document.getElementById('root')
+  </Router>
 );
 
